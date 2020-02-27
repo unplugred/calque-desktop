bf7a7e527aab1d8317a694a1775aea32858f08a4
melody <melody@unplug.red>
2020-02-27 18:28:18 +0200

    delayed startup on linux to fix transparency

--- app/run.js
+++ app/run.js
@@ -1,6 +1,13 @@
 var {app, BrowserWindow} = require('electron');
 
 app.on('ready', function(){
+    if(process.platform === "win32" || process.platform === "darwin")
+        openwin();
+    else
+        setTimeout(openwin, 500);
+});
+
+function openwin(){
     win = new BrowserWindow({
         title: "Calque",
         icon: "favicon.png",
@@ -10,4 +17,4 @@ app.on('ready', function(){
         webPreferences: { nodeIntegration: true }
     });
     win.loadURL("file://" + __dirname + "/index.html");
-});
+};
