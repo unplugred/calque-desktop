f7699ea4df05560388261312eabb1677ca4b6bdc
melody <melody@unplug.red>
2020-02-27 15:34:59 +0200

    desktopified

---
+++ app/run.js
@@ -0,0 +1,11 @@
+var {app, BrowserWindow} = require('electron');
+
+app.on('ready', function(){
+	win = new BrowserWindow({
+		title: "Calque",
+		icon: "favicon.png",
+		darkTheme: true,
+		webPreferences: { nodeIntegration: true }
+	});
+	win.loadURL("file://" + __dirname + "/index.html");
+});
