9e24953cfda0bd06e242f96bc42b87a17adf6c4a
melody <melody@unplug.red>
2020-02-27 16:19:25 +0200

    unified indentation

--- app/run.js
+++ app/run.js
@@ -1,13 +1,13 @@
 var {app, BrowserWindow} = require('electron');
 
 app.on('ready', function(){
-	win = new BrowserWindow({
-		title: "Calque",
-		icon: "favicon.png",
-		frame: false,
-		transparent: true,
-		darkTheme: true,
-		webPreferences: { nodeIntegration: true }
-	});
-	win.loadURL("file://" + __dirname + "/index.html");
+    win = new BrowserWindow({
+        title: "Calque",
+        icon: "favicon.png",
+        frame: false,
+        transparent: true,
+        darkTheme: true,
+        webPreferences: { nodeIntegration: true }
+    });
+    win.loadURL("file://" + __dirname + "/index.html");
 });
