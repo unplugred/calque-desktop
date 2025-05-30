b0cd67f495b40b0a2eaf6e55bce7695c74ced59b
melody <melody@unplug.red>
2025-05-30 12:17:58 +0300

    modernization

--- app/run.js
+++ app/run.js
@@ -1,64 +1,89 @@
 var {app, BrowserWindow, screen} = require('electron');
+require('@electron/remote/main').initialize();
+var path = require('path');
+var fs = require('fs');
+
+datapath = path.join(app.getPath('userData'),'calque.json');
+try {
+	global.data = JSON.parse(fs.readFileSync(datapath));
+} catch(error) {
+	global.data = {
+		width: 400,
+		height: 600,
+		theme: 0,
+		isontop: false,
+		input: null
+	};
+}
+global.savedata = function() {
+	fs.writeFileSync(datapath,JSON.stringify(global.data));
+}
 
 app.on('ready', function(){
-    if(process.platform === "win32" || process.platform === "darwin")
-        openwin();
-    else
-        setTimeout(openwin, 500);
+	if(process.platform === "win32" || process.platform === "darwin")
+		openwin();
+	else
+		setTimeout(openwin, 500);
 });
 
 function openwin(){
-    global.win = new BrowserWindow({
-        title: "Calque",
-        icon: "icons/icon64.png",
-        frame: false,
-        transparent: true,
-        darkTheme: true,
-        webPreferences: {
-			nodeIntegration: true,
-			enableRemoteModule: true,
-			contextIsolation: false
-		}
-    });
-    global.win.loadURL("file://" + __dirname + "/index.html");
-    global.cs = new BrowserWindow({
-        title: "Cheat Sheet",
-        icon: "icons/icon64.png",
-        frame: false,
-        transparent: true,
-        darkTheme: true,
-        webPreferences: {
+	global.win = new BrowserWindow({
+		title: "Calque",
+		icon: "icons/icon64.png",
+		frame: false,
+		transparent: true,
+		darkTheme: true,
+		webPreferences: {
 			nodeIntegration: true,
 			enableRemoteModule: true,
 			contextIsolation: false
 		},
-        width: 388,
-        minWidth: 388,
-        maxWidth: 388,
-        show: false
-    });
-    global.cs.webContents.on('new-window', function(e, url) {
-        e.preventDefault();
-        require('electron').shell.openExternal(url);
-    });
-    global.cs.loadURL("file://" + __dirname + "/cheatsheet.html");
+		width: global.data.width,
+		height: global.data.height
+	});
+	global.win.on('resize',() => {
+		let { width, height } = global.win.getBounds();
+		global.data.width = width;
+		global.data.height = height;
+		global.savedata();
+	});
+	require('@electron/remote/main').enable(global.win.webContents);
+	global.win.loadURL("file://" + __dirname + "/index.html");
+	global.cs = new BrowserWindow({
+		title: "Cheat Sheet",
+		icon: "icons/icon64.png",
+		frame: false,
+		transparent: true,
+		darkTheme: true,
+		webPreferences: {
+			nodeIntegration: true,
+			enableRemoteModule: true,
+			contextIsolation: false
+		},
+		width: 388,
+		minWidth: 388,
+		maxWidth: 388,
+		show: false
+	});
+	require('@electron/remote/main').enable(global.cs.webContents);
+	global.cs.webContents.on('new-window', function(e, url) {
+		e.preventDefault();
+		require('electron').shell.openExternal(url);
+	});
+	global.cs.loadURL("file://" + __dirname + "/cheatsheet.html");
 };
 
 openedcheatsheet = false;
 global.cheatsheet = function(){
-    if(!openedcheatsheet) {
-        let bounds = global.win.getBounds();
-        let workarea = screen.getDisplayNearestPoint(bounds).workArea;
-        global.cs.setBounds({
-            height: bounds.height,
-            y: bounds.y,
-            x: Math.min(bounds.x + bounds.width, workarea.x + workarea.width - 388)
-        });
-        openedcheatsheet = true;
-    }
-    global.cs.show();
-}
-global.closee = function(){
-    global.win.close();
-    global.cs.close();
+	if(!openedcheatsheet) {
+		let bounds = global.win.getBounds();
+		let workarea = screen.getDisplayNearestPoint(bounds).workArea;
+		global.cs.setBounds({
+			height: bounds.height,
+			y: bounds.y,
+			x: Math.min(bounds.x + bounds.width, workarea.x + workarea.width - 388)
+		});
+		openedcheatsheet = true;
+	}
+	global.cs.show();
 }
