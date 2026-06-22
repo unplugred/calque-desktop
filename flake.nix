3fa047a89600a7218df1a20a62c44bedff18719a
melody <melody@unplug.red>
2026-06-22 23:16:05 +0300

    nix flake build

--- flake.nix
+++ flake.nix
@@ -8,17 +8,38 @@
 	flake-utils.lib.eachDefaultSystem (system:
 	let
 		pkgs = nixpkgs.legacyPackages.${system};
-	in {
+	in with pkgs; {
 		devShells.default = pkgs.mkShell {
-			buildInputs = with pkgs; [ # packages
+			buildInputs = [ # packages
 				git
 				nodejs
 				electron
 			];
 
 			# environment variables
-			ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 			PROJECT_NAME = "calque";
+		};
+		packages.default = buildNpmPackage rec {
+			name = "Calque";
+			version = "1.5.1";
+			src = ./app;
+			npmDepsHash = "sha256-PB/DPRusZSDfeUO4dtXu1rEwYnYVq8/pTEh/eCQxE0s=";
+
+			packageJSON = ./app/package.json;
+			nativeBuildInputs = [
+				electron
+			];
+
+			dontNpmBuild = true;
+
+			env = {
+				ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
+			};
+
+			postInstall = ''
+				makeWrapper ${electron}/bin/electron $out/bin/${name} \
+					--add-flags $out/lib/node_modules/${name}/run.js
+			'';
 		};
 	});
 }
